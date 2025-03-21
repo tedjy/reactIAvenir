fefe77096ebe714b0c398c4d234c278c1c0f6b37
williot <williotteddy9@gmail.com>
2025-03-21 23:31:57 +0100

    SliderETP

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -4,50 +4,27 @@ import { motion } from "framer-motion";
 export const InfiniteSlider = (props) => {
   if (!props.data) return <div>loading...</div>;
 
-  const Slider = [...props.data, ...props.data]; // duplication pour le scroll infini
+  const Slider = [...props.data, ...props.data , ...props.data]; // duplication pour le scroll infini
 
   return (
-    <div id="Slider" className="text-center">
-    <div className="container">
-    <div className="col-md-8 col-md-offset-2 section-title">
-      <h2>Entreprises partenaires</h2>
+    <div id="slider" className="slider-container">
+      <div className="container">
+        <div className="section-title">
+          <h2>Entreprises partenaires</h2>
+        </div>
+
+        {/* Conteneur du slider */}
+        <div className="slider-wrapper">
+          <div className="slider-track">
+            {/* Duplication pour effet infini */}
+            {Slider.concat(props.data).map((d, i) => (
+              <div key={`${d.name}-${i}`} className="slide">
+                <img src={d.img} alt={d.name} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-    <div id="row">
-    {/* Conteneur qui force une seule ligne */}
-    <div className="">
-      <motion.div
-        className="sliderContent"
-        initial={{x:"-15%"}}
-        animate={{x: "-115%"}}
-        transition={{
-          repeat: Infinity,
-          repeatType: "loop",
-          ease: "linear",
-          duration: 30,
-        }}
-      >
-        {Slider.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="cardsLine ">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="" />
-  
-                  </div>
-                </div>
-              ))}
-              {Slider.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="cardsLine ">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
- 
-                  </div>
-                </div>
-              ))}
-      </motion.div>
-    </div>
-    </div>
-    </div>
-  </div>
   );
 };
